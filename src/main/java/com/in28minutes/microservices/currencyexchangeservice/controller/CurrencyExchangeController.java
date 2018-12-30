97824d5dfc5992eb45a0ca89e6bc7e0ed4cd56f7
rakesh kapri <rakeshkapri@gmail.com>
2018-12-30 12:43:05 +0000

    Added Spring Sleuth for assining request ids

--- src/main/java/com/in28minutes/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
+++ src/main/java/com/in28minutes/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
@@ -2,6 +2,8 @@ package com.in28minutes.microservices.currencyexchangeservice.controller;
 
 import com.in28minutes.microservices.currencyexchangeservice.entity.CurrencyValue;
 import com.in28minutes.microservices.currencyexchangeservice.repository.CurrencyRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +14,8 @@ import java.util.Optional;
 
 @RestController
 public class CurrencyExchangeController {
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @Autowired
     Environment environment;
 
@@ -20,9 +24,11 @@ public class CurrencyExchangeController {
 
     @GetMapping(value = "/currency-exchange/from/{from}/to/{to}")
     public CurrencyValue getCurrencyValue(@PathVariable String from, @PathVariable String to){
+        logger.info("Inside Currency exchange service");
         Optional<CurrencyValue> currencyValueOp = currencyRepository.findByFromAndTo(from.toUpperCase(), to.toUpperCase());
         CurrencyValue currencyValue = currencyValueOp.get();
         currencyValue.setPort(Integer.parseInt(environment.getProperty("server.port")));
+        logger.info("Returning response from exchange service --> {}", currencyValue);
         return currencyValue;
     }
 }
